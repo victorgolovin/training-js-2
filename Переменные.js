eb5c7232e62b7e36e272f98d6e788d47cd7023bc
Victor Golovin <rinoally@gmail.com>
2023-05-24 10:28:04 +1000

    lesson -variables

---
+++ Переменные.js
@@ -0,0 +1,19 @@
+// Что такое переменная 
+let NumberOne = 1;
+
+console.log(NumberOne);
+
+// Как используется перменная для хранения данных
+
+
+// Объявление переменной 
+let numberTwo;
+
+// Инициализация переменной
+numberTwo = 2;
+
+console.log(numberTwo);
+// Изменение переменной
+numberTwo = 3;
+
+console.log(numberTwo);
